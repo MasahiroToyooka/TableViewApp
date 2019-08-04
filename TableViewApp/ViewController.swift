e679104a6d19c1011f5ef2e29f29ff62d3120d4a
MasahiroToyooka <ehara2481@gmail.com>
2019-08-04 14:13:35 +0900

    Initial Commit

---
+++ TableViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by 豊岡正紘 on 2019/08/04.
+//  Copyright © 2019 Masahiro Toyooka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
